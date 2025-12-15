805a920eeea03b2c75fa794533d423bfe805ddb2
mint <nahithack@gmail.com>
2025-12-15 14:20:34 +0100

    install docker compose plugin as part of start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root user"
+  exit 1
+fi
+
 set -e
+
+# Install docker compose plugin
+echo "[*] Installing the docker compose plugin"
+
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+apt install docker-compose-plugin
+
+echo "[*] Docker compose plugin installed"
+
 
 echo "[*] Stopping existing containers and removing volumes..."
 docker compose down -v --remove-orphans
--- stop.sh
+++ stop.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root user"
+  exit 1
+fi
+
 set -e
 
 echo "[*] Stopping and removing containers/volumes"
